a209e9d46d1219df1ee55f0105e58557febf563a
Andrew Velez <andrewvelez@outlook.com>
2026-07-03 18:20:33 -0400

    pulling path logic out into separate script

---
+++ stow/home/.config/bash/path.bash
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+#  @author Andrew Velez 2026
+# ~/.config/bash/path.bash
+
+add_dir_path() {
+    if [ -z "$1" ] || ! [ -d "$2" ]; then
+        return 1
+    fi
+
+    current="${!1}"
+
+    case ":${!1}:" in
+        *":${2}:"*) ;;
+        *) printf -v "$1" '%s:%s' "$2" "${!1}" ;;
+    esac
+}
+export -f "add_dir_path"
+
+add_dir_path "PATH" "$HOME/.local/bin"
+add_dir_path "PATH" "$HOME/.local/scripts"
+add_dir_path "PATH" "$HOME/AppImages"
+add_dir_path "PATH" "$DOTNET_ROOT"
+add_dir_path "PATH" "$DOTNET_ROOT/tools"
+add_dir_path "PATH" "$RUBY_HOME/bin"
+add_dir_path "PATH" "/usr/lib/go/bin"
+add_dir_path "PATH" "$GOPATH/bin"
+add_dir_path "PATH" "$CARGO_HOME/bin"
+add_dir_path "PATH" "$BUN_INSTALL/bin"
+add_dir_path "PATH" "$HOME/.local/radicle/bin"
+
+export PATH
